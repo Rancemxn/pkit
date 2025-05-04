8190bb176e5b1356f0c026b1fa4fb996b4950fee
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2025-05-04 13:17:34 +0800

    feat(vgmstream): import vgm

--- src/phicain/main.py
+++ src/phicain/main.py
@@ -1,1 +1,2 @@
 import utils.ffmpeg_crosser  # noqa: F401
+import utils.vgmstream_fixer  # noqa: F401
---
+++ src/phicain/utils/vgmstream_fixer/__init__.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+from os.path import join
+from loguru import logger
+
+from ..libcheck import cmd
+from ..syscheck import check_android
+
+vgm_bin_path: str = "vgmstream_cli"
+
+if check_android():
+    logger.info("Android Platform detected. Vgmstream Path Fix Start.")
+    from android import mActivity  # type: ignore
+
+    app_info = mActivity.getApplicationInfo()
+    native_lib_dir: str = app_info.nativeLibraryDir
+    vgm_bin_path = join(native_lib_dir, "vgmstream_cli")
+    logger.info(f"Found Vgmstream_cli locate in {vgm_bin_path}")
+
+    vgmdir = join(native_lib_dir, "vgmstream")
+    os.makedirs(vgmdir, exist_ok=True)
+
+    built_libraries = [
+        "vgmstream_cli",
+        "libvgmstream.so",
+        "libvorbis_vgm.so",
+        "libvorbisfile_vgm.so",
+        "libogg_vgm.so",
+        "libavcodec_vgm.so",
+        "libavformat_vgm.so",
+        "libavutil_vgm.so",
+        "libswresample_vgm.so",
+    ]
+    for lib in built_libraries:
+        shutil.move(lib, join(vgmdir, lib))
+
+    vgm_bin_path = join(vgmdir, "vgmstream_cli")
+
+    logger.info(f"Move to {vgm_bin_path}")
+
+    os.environ["LD_LIBRARY_PATH"] = (
+        vgmdir + os.pathsep + os.environ.get("LD_LIBRARY_PATH", "")
+    )
+
+    logger.debug(f"LD_LIBRARY_PATH update to: {os.environ.get('LD_LIBRARY_PATH', '')}")
+
+    logger.info("LD_LIBRARY_PATH Patch Done.")
+
+logger.info("Test Vgmstream")
+
+cmd([vgm_bin_path])
+
+cmd([vgm_bin_path, "-version"])
