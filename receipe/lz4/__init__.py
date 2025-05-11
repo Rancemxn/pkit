ad0283c7e8e9f8a356c0161b0b7e07877ea74f9d
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2025-05-11 10:20:16 +0800

    Update __init__.py

--- receipe/lz4/__init__.py
+++ receipe/lz4/__init__.py
@@ -9,10 +9,15 @@ class Lz4Recipe(PythonRecipe):
 
     depends = ["python3", "setuptools"]
 
-    hostpython_depends = ["setuptools", "wheel", "pkgconfig"]
+    hostpython_depends = ["setuptools", "wheel", "pkgconfig", "setuptools_scm"]
 
     call_hostpython_via_targetpython = False
     install_in_hostpython = False
+
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        env["PYLZ4_USE_SYSTEM_LZ4"] = "0"
+        return env
 
     def build_arch(self, arch):
         super().build_arch(arch)
