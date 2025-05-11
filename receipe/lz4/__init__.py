f56c5e6412274de270d5108991fa716bc908d75d
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2025-05-11 09:36:53 +0800

    Create __init__.py

---
+++ receipe/lz4/__init__.py
@@ -0,0 +1,19 @@
+from pythonforandroid.recipe import PythonRecipe  # type: ignore
+
+
+class Lz4Recipe(PythonRecipe):
+    version = "v4.4.4"
+    url = "https://github.com/python-lz4/python-lz4/archive/refs/tags/{version}.zip"
+
+    name = "lz4"
+
+    depends = ["python3", "setuptools"]
+
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = False
+
+    def build_arch(self, arch):
+        super().build_arch(arch)
+
+
+recipe = Lz4Recipe()
