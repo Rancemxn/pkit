3d0434cf48ef3ad1bdd3a6f600a26a8b1e7d5198
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2025-05-11 11:04:46 +0800

    Update __init__.py

--- receipe/lz4/__init__.py
+++ receipe/lz4/__init__.py
@@ -22,6 +22,7 @@ class Lz4Recipe(PythonRecipe):
 
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super().get_recipe_env(arch, with_flags_in_cc)
+        env["SETUPTOOLS_SCM_PRETEND_VERSION_FOR_LZ4"] = self.version
         env["PYLZ4_USE_SYSTEM_LZ4"] = "0"
         return env
 
