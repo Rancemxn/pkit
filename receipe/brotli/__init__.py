2e70d157a1f05e22177619d10aebc8f15b7ca596
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2025-05-16 21:51:46 +0800

    fix

--- receipe/brotli/__init__.py
+++ receipe/brotli/__init__.py
@@ -2,13 +2,18 @@ from pythonforandroid.recipe import PyProjectRecipe  # type: ignore
 
 
 class BrotliRecipe(PyProjectRecipe):
-    version = "4.4.4"
+    version = "1.1.0"
 
-    url = "https://github.com/python-lz4/python-lz4/archive/refs/tags/v{version}.tar.gz"
+    url = "https://github.com/google/brotli/archive/refs/tags/v{version}.tar.gz"
 
     name = "brotli"
 
     site_packages_name = "brotli"
+
+    def get_recipe_env(self, arch, *args, **kwargs):
+        env = super().get_recipe_env(arch, *args, **kwargs)
+        env["SETUPTOOLS_SCM_PRETEND_VERSION_FOR_BROTLI"] = self.version
+        return env
 
 
 recipe = BrotliRecipe()
