dbf0a04eaa4f2ff8ab97fc30ff7e8999e79115eb
Rancemxn <209220634+Rancemxn@users.noreply.github.com>
2025-05-16 21:26:43 +0800

    Update __init__.py

--- receipe/lz4/__init__.py
+++ receipe/lz4/__init__.py
@@ -1,18 +1,21 @@
-from pythonforandroid.recipe import CompiledComponentsPythonRecipe  # type: ignore
+from pythonforandroid.recipe import PyProjectRecipe  # type: ignore
 
 
-class LZ4Recipe(CompiledComponentsPythonRecipe):
+class LZ4Recipe(PyProjectRecipe):
     version = "4.4.4"
 
     url = "https://github.com/python-lz4/python-lz4/archive/refs/tags/v{version}.tar.gz"
 
     name = "lz4"
 
-    depends = ["setuptools"]
-
     call_hostpython_via_targetpython = False
 
     site_packages_name = "lz4"
+
+    def get_recipe_env(self, arch, *args, **kwargs):
+        env = super().get_recipe_env(arch, *args, **kwargs)
+        env["PYLZ4_USE_SYSTEM_LZ4"] = "0"
+        return env
 
 
 recipe = LZ4Recipe()
